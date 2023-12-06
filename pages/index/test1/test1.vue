3c5eb63991d3686bb04a54a96636523bbc02b040
mao <1296193245@qq.com>
2023-12-06 14:56:52 +0800

    uniapp-navigator-component

---
+++ pages/index/test1/test1.vue
@@ -0,0 +1,16 @@
+<template>
+	<view>
+		<navigator url="/pages/index/index" hover-class="navigator-hover">
+			<button type="default">跳转到新页面</button>
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+	}
+</script>
+
+<style>
+
+</style>
